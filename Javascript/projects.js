a4e5e3dd15ca303b39c6ff330a7da674accc43c4
JGibson37 <Jason.Gibson15@yahoo.com>
2020-08-08 12:29:43 -0400

    added modals to projects

---
+++ Javascript/projects.js
@@ -0,0 +1,51 @@
+
+
+const addEventListeners = () =>{
+    const kata = document.getElementById("kata")
+    kata.addEventListener("click", () =>{
+        let kataModal = document.getElementById("kata-modal")
+        kataModal.style.display = "block"
+        let span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", () =>{
+            kataModal.style.display= "none"
+        })
+    })
+    const virutalPet = document.getElementById("pet")
+    virutalPet.addEventListener("click", () =>{
+        let petModal = document.getElementById("pet-modal")
+        petModal.style.display = "block"
+        let span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", () =>{
+            petModal.style.display= "none"
+        })
+    })
+    const codey = document.getElementById("codey")
+    codey.addEventListener("click", () =>{
+        let codeyModal = document.getElementById("codey-modal")
+        codeyModal.style.display = "block"
+        let span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", () =>{
+            codeyModal.style.display= "none"
+        })
+    })
+    const portfolio = document.getElementById("portfolio")
+    portfolio.addEventListener("click", () =>{
+        let portfolioModal = document.getElementById("portfolio-modal")
+        portfolioModal.style.display = "block"
+        let span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", () =>{
+            portfolioModal.style.display= "none"
+        })
+    })
+    const artists = document.getElementById("artists")
+    artists.addEventListener("click", () =>{
+        let artistsModal = document.getElementById("artists-modal")
+        artistsModal.style.display = "block"
+        let span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", () =>{
+            artistsModal.style.display= "none"
+        })
+    })
+}
+
+addEventListeners()
